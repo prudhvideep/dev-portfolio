76f6ae4eb1aa9d7cd628cf89e64006a023d43eaf
prudhvi deep <prudhvi@clucoud.com>
2024-12-16 21:40:25 -0700

    Updated Resume

--- src/pages/Resume.tsx
+++ src/pages/Resume.tsx
@@ -18,7 +18,7 @@ export default function Experience() {
                 <p className="text-gray-600">
                   <span className="text-sm italic font-normal">2023</span>
                   <span> - </span>
-                  <span className="italic text-sm font-normal">Present</span>
+                  <span className="italic text-sm font-normal">May 2025 <span className="italian">(Expected)</span></span>
                 </p>
               </div>
             </div>
@@ -97,9 +97,100 @@ export default function Experience() {
         </ul>
       </div>
       <div className="mt-10">
-        <h1 className="font-[450] text-gray-900 mb-4 text-xl">
+        <h1 className="font-[500] text-gray-900 mb-4 text-xl text-opacity-100">
           Professional Experience
         </h1>
+        <div className="flex flex-row items-center justify-between">
+          <div className="text-lg font-normal text-gray-900">
+            Project Leader, Oracle Cloud
+          </div>
+          <div>
+            <p className="text-gray-600">
+              <span className="text-sm italic font-normal">Feb 2023</span>
+              <span> - </span>
+              <span className="italic text-sm font-normal">Aug 2023</span>
+            </p>
+          </div>
+        </div>
+        <ul className="mt-2 list-disc ml-8">
+          <li className="list-item text-gray-600 text-md">
+            Led development of a scalable Go-based customer engagement platform
+            for Oracle Engagement Cloud, overcoming distributed system
+            challenges and achieving 99.9% uptime, 30% reduced latency, and
+            enhanced response personalization.
+          </li>
+          <li className="list-item text-gray-600">
+            Achieved seamless API migration with zero downtime by validating the
+            key performance metrics for the new APIs using a combination of
+            traffic replay, sticky canaries and A/B testing.
+          </li>
+          <li className="list-item text-gray-600">
+            Optimized data archiving and retrieval for applications in Oracle
+            cloud by automating data migration from high-cost storage to
+            archival tiers based on usage patterns, reducing storage costs by
+            11%.
+          </li>
+        </ul>
+        <div className="mt-4 flex flex-row items-center justify-between">
+          <div className="text-lg font-normal text-gray-900">
+            Senior Application Developer, Oracle Cloud
+          </div>
+          <div>
+            <p className="text-gray-600">
+              <span className="text-sm italic font-normal">Dec 2020</span>
+              <span> - </span>
+              <span className="italic text-sm font-normal">Feb 2023</span>
+            </p>
+          </div>
+        </div>
+        <ul className="mt-2 list-disc ml-8">
+          <li className="list-item text-gray-600 text-md">
+            Designed, proposed and rolled out a revamped, containerized build
+            infrastructure for CI/CD using Docker, Kubernetes and Jenkins over 8
+            months, migrating over 300 jobs used by over 100 developers
+          </li>
+          <li className="list-item text-gray-600">
+            Implemented circuit breakers using Resilience4j and fallback
+            mechanisms, reducing service outage incidents by 23%, resulting in a
+            more fault-tolerant microservices architecture.
+          </li>
+          <li className="list-item text-gray-600">
+            Enriched system metrics by integrating the platforms with telemetry,
+            facilitating logging by correlating APIs with vector contexts
+          </li>
+        </ul>
+        <div className="mt-4 flex flex-row items-center justify-between">
+          <div className="text-lg font-normal text-gray-900">
+            Application Developer, Oracle Cloud
+          </div>
+          <div>
+            <p className="text-gray-600">
+              <span className="text-sm italic font-normal">June 2018</span>
+              <span> - </span>
+              <span className="italic text-sm font-normal">Dec 2020</span>
+            </p>
+          </div>
+        </div>
+        <ul className="mt-2 list-disc ml-8">
+          <li className="list-item text-gray-600 text-md">
+            Developed cloud-native applications of the Oracle SCM platform that
+            are scalable, distributed, and highly available, using Java, React,
+            Oracle DB, Pl/Sql, Oracle Lambda, and OpenShift.
+          </li>
+          <li className="list-item text-gray-600">
+            Introduced Redux for centralized state management, reducing UI
+            latency by 19% and ensuring consistent real-time updates across
+            diverse UI components.
+          </li>
+          <li className="list-item text-gray-600">
+            Built REST APIs from scratch, architecting scalable and
+            fault-tolerant solutions from the ground up using Spring Boot.
+          </li>
+          <li className="list-item text-gray-600">
+            Completely automated the inventory platforms’ user experience
+            testing process by integrating Selenium webdriver.
+          </li>
+        </ul>
       </div>
       <div className="mt-16 p-6 w-full border border-gray-300 bg-white rounded-lg shadow-lg">
         <div className="flex flex-row items-center justify-center space-x-6">
